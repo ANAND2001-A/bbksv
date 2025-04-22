4f171d9021b6845c6ce44c8fe67a7a85d628d4f5
Anand Bahadur Yadav <75464458+ANAND2001-A@users.noreply.github.com>
2025-04-22 14:26:32 +0530

    save

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -11,8 +11,10 @@ const CustomButton = ({
     <button
       type={type}
       onClick={onClick}
-      className={`text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${className}`}
-      style={{ backgroundColor: bgColor }} // Dynamically set the background color if needed
+      className={`text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl 
+        focus:outline-none focus:ring-0 border-0 
+        font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${className}`}
+      style={{ backgroundColor: bgColor }}
     >
       {children}
     </button>
