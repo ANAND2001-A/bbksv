58ebf0ce78511764e31922de6b560113ef05e831
Anand Bahadur Yadav <75464458+ANAND2001-A@users.noreply.github.com>
2025-04-22 12:29:57 +0530

    save

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
         <div className="bg-blue-50 text-[#005B96] rounded-full px-6 py-2 flex items-center gap-6 shadow-sm relative">
           <Link to="/" className="font-medium">Home</Link>
 
-          {/* Courses Dropdown */}
+          {/* Coursesbdshjsdjhajhfghjb Dropdown */}
           <div className="relative" ref={landingsRef}>
             <div
               onClick={() => setShowLandings(!showLandings)}
